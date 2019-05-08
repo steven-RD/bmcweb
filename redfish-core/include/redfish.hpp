8b2a6e54c30fb5ded22d9e902488723d585a6094
Steven <bao_gang_168@163.com>
2019-05-08 15:37:02 +0800

    modify  redfish-core to add usi Firmware module

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-*/
+ */
 #pragma once
 
 #include "../lib/account_service.hpp"
@@ -29,92 +29,101 @@
 #include "../lib/systems.hpp"
 #include "../lib/thermal.hpp"
 #include "../lib/update_service.hpp"
+#include "../lib/usi_firmware.hpp"
 #include "webserver_common.hpp"
 
-namespace redfish
-{
-/*
- * @brief Top level class installing and providing Redfish services
- */
-class RedfishService
-{
-  public:
+namespace redfish {
+
     /*
-     * @brief Redfish service constructor
-     *
-     * Loads Redfish configuration and installs schema resources
-     *
-     * @param[in] app   Crow app on which Redfish will initialize
+     * @brief Top level class installing and providing Redfish services
      */
-    RedfishService(CrowApp& app)
-    {
-        nodes.emplace_back(std::make_unique<AccountService>(app));
-        nodes.emplace_back(std::make_unique<AccountsCollection>(app));
-        nodes.emplace_back(std::make_unique<ManagerAccount>(app));
-        nodes.emplace_back(std::make_unique<SessionCollection>(app));
-        nodes.emplace_back(std::make_unique<Roles>(app));
-        nodes.emplace_back(std::make_unique<RoleCollection>(app));
-        nodes.emplace_back(std::make_unique<ServiceRoot>(app));
-        nodes.emplace_back(std::make_unique<NetworkProtocol>(app));
-        nodes.emplace_back(std::make_unique<SessionService>(app));
-        nodes.emplace_back(std::make_unique<EthernetCollection>(app));
-        nodes.emplace_back(std::make_unique<EthernetInterface>(app));
-        nodes.emplace_back(std::make_unique<Thermal>(app));
-        nodes.emplace_back(std::make_unique<ManagerCollection>(app));
-        nodes.emplace_back(std::make_unique<Manager>(app));
-        nodes.emplace_back(std::make_unique<ManagerActionsReset>(app));
-        nodes.emplace_back(std::make_unique<Power>(app));
-        nodes.emplace_back(std::make_unique<ChassisCollection>(app));
-        nodes.emplace_back(std::make_unique<Chassis>(app));
-        nodes.emplace_back(std::make_unique<UpdateService>(app));
-        nodes.emplace_back(std::make_unique<SoftwareInventoryCollection>(app));
-        nodes.emplace_back(std::make_unique<SoftwareInventory>(app));
-        nodes.emplace_back(
-            std::make_unique<VlanNetworkInterfaceCollection>(app));
-        nodes.emplace_back(std::make_unique<VlanNetworkInterface>(app));
+    class RedfishService {
+    public:
 
-        nodes.emplace_back(std::make_unique<SystemLogServiceCollection>(app));
-        nodes.emplace_back(std::make_unique<EventLogService>(app));
-        nodes.emplace_back(std::make_unique<EventLogEntryCollection>(app));
-        nodes.emplace_back(std::make_unique<EventLogEntry>(app));
+        /*
+         * @brief Redfish service constructor
+         *
+         * Loads Redfish configuration and installs schema resources
+         *
+         * @param[in] app   Crow app on which Redfish will initialize
+         */
+        RedfishService(CrowApp& app) {
+            
+            /*
+             * Add USI redfish class modules
+             */
+            nodes.emplace_back(std::make_unique<FirmwareService>(app));
+            nodes.emplace_back(std::make_unique<Functionl>(app));
+            nodes.emplace_back(std::make_unique<Ready>(app));
+            //nodes.emplace_back(std::make_unique<Update>(app));
+            //nodes.emplace_back(std::make_unique<Activate>(app));
+            
+            nodes.emplace_back(std::make_unique<AccountService>(app));
+            nodes.emplace_back(std::make_unique<AccountsCollection>(app));
+            nodes.emplace_back(std::make_unique<ManagerAccount>(app));
+            nodes.emplace_back(std::make_unique<SessionCollection>(app));
+            nodes.emplace_back(std::make_unique<Roles>(app));
+            nodes.emplace_back(std::make_unique<RoleCollection>(app));
+            nodes.emplace_back(std::make_unique<ServiceRoot>(app));
+            nodes.emplace_back(std::make_unique<NetworkProtocol>(app));
+            nodes.emplace_back(std::make_unique<SessionService>(app));
+            nodes.emplace_back(std::make_unique<EthernetCollection>(app));
+            nodes.emplace_back(std::make_unique<EthernetInterface>(app));
+            nodes.emplace_back(std::make_unique<Thermal>(app));
+            nodes.emplace_back(std::make_unique<ManagerCollection>(app));
+            nodes.emplace_back(std::make_unique<Manager>(app));
+            nodes.emplace_back(std::make_unique<ManagerActionsReset>(app));
+            nodes.emplace_back(std::make_unique<Power>(app));
+            nodes.emplace_back(std::make_unique<ChassisCollection>(app));
+            nodes.emplace_back(std::make_unique<Chassis>(app));
+            nodes.emplace_back(std::make_unique<UpdateService>(app));
+            nodes.emplace_back(std::make_unique<SoftwareInventoryCollection>(app));
+            nodes.emplace_back(std::make_unique<SoftwareInventory>(app));
+            nodes.emplace_back(
+                    std::make_unique<VlanNetworkInterfaceCollection>(app));
+            nodes.emplace_back(std::make_unique<VlanNetworkInterface>(app));
 
-        nodes.emplace_back(std::make_unique<BMCLogServiceCollection>(app));
+            nodes.emplace_back(std::make_unique<SystemLogServiceCollection>(app));
+            nodes.emplace_back(std::make_unique<EventLogService>(app));
+            nodes.emplace_back(std::make_unique<EventLogEntryCollection>(app));
+            nodes.emplace_back(std::make_unique<EventLogEntry>(app));
+
+            nodes.emplace_back(std::make_unique<BMCLogServiceCollection>(app));
 #ifdef BMCWEB_ENABLE_REDFISH_BMC_JOURNAL
-        nodes.emplace_back(std::make_unique<BMCJournalLogService>(app));
-        nodes.emplace_back(std::make_unique<BMCJournalLogEntryCollection>(app));
-        nodes.emplace_back(std::make_unique<BMCJournalLogEntry>(app));
+            nodes.emplace_back(std::make_unique<BMCJournalLogService>(app));
+            nodes.emplace_back(std::make_unique<BMCJournalLogEntryCollection>(app));
+            nodes.emplace_back(std::make_unique<BMCJournalLogEntry>(app));
 #endif
 
 #ifdef BMCWEB_ENABLE_REDFISH_CPU_LOG
-        nodes.emplace_back(std::make_unique<CrashdumpService>(app));
-        nodes.emplace_back(std::make_unique<CrashdumpEntryCollection>(app));
-        nodes.emplace_back(std::make_unique<CrashdumpEntry>(app));
-        nodes.emplace_back(std::make_unique<OnDemandCrashdump>(app));
+            nodes.emplace_back(std::make_unique<CPULogService>(app));
+            nodes.emplace_back(std::make_unique<CPULogEntryCollection>(app));
+            nodes.emplace_back(std::make_unique<CPULogEntry>(app));
+            nodes.emplace_back(std::make_unique<OnDemandCPULog>(app));
 #ifdef BMCWEB_ENABLE_REDFISH_RAW_PECI
-        nodes.emplace_back(std::make_unique<SendRawPECI>(app));
+            nodes.emplace_back(std::make_unique<SendRawPECI>(app));
 #endif // BMCWEB_ENABLE_REDFISH_RAW_PECI
 #endif // BMCWEB_ENABLE_REDFISH_CPU_LOG
 
-        nodes.emplace_back(std::make_unique<ProcessorCollection>(app));
-        nodes.emplace_back(std::make_unique<Processor>(app));
-        nodes.emplace_back(std::make_unique<MemoryCollection>(app));
-        nodes.emplace_back(std::make_unique<Memory>(app));
+            nodes.emplace_back(std::make_unique<ProcessorCollection>(app));
+            nodes.emplace_back(std::make_unique<Processor>(app));
+            nodes.emplace_back(std::make_unique<MemoryCollection>(app));
+            nodes.emplace_back(std::make_unique<Memory>(app));
 
-        nodes.emplace_back(std::make_unique<SystemsCollection>(app));
-        nodes.emplace_back(std::make_unique<Systems>(app));
-        nodes.emplace_back(std::make_unique<SystemActionsReset>(app));
+            nodes.emplace_back(std::make_unique<SystemsCollection>(app));
+            nodes.emplace_back(std::make_unique<Systems>(app));
+            nodes.emplace_back(std::make_unique<SystemActionsReset>(app));
 #ifdef BMCWEB_ENABLE_REDFISH_DBUS_LOG_ENTRIES
-        nodes.emplace_back(std::make_unique<DBusLogServiceActionsClear>(app));
+            nodes.emplace_back(std::make_unique<DBusLogServiceActionsClear>(app));
 #endif
 
-        for (const auto& node : nodes)
-        {
-            node->initPrivileges();
+            for (const auto& node : nodes) {
+                node->initPrivileges();
+            }
         }
-    }
 
-  private:
-    std::vector<std::unique_ptr<Node>> nodes;
-};
+    private:
+        std::vector<std::unique_ptr<Node>> nodes;
+    };
 
 } // namespace redfish
