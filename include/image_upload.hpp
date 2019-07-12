988796e7150eb45fb178707dcf55699457d20d87
Steven <bao_gang_168@163.com>
2019-07-12 14:29:12 +0800

    modify upload filename

--- include/image_upload.hpp
+++ include/image_upload.hpp
@@ -103,8 +103,7 @@ inline void uploadImageHandler(const crow::Request& req, crow::Response& res,
         callback);
 
     std::string filepath(
-        "/tmp/images/" +
-        boost::uuids::to_string(boost::uuids::random_generator()()));
+        "/tmp/images/" + filename);
     BMCWEB_LOG_DEBUG << "Writing file to " << filepath;
     std::ofstream out(filepath, std::ofstream::out | std::ofstream::binary |
                                     std::ofstream::trunc);
