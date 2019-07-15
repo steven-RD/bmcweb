8c2d2bea3f7ba342be73982fe8d9c5e8216b044e
Steven <bao_gang_168@163.com>
2019-07-15 17:01:04 +0800

    add funtion to tar

--- include/usi_switch_upload.hpp
+++ include/usi_switch_upload.hpp
@@ -23,6 +23,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stdio.h>
 
 namespace crow {
     namespace usi_switch_upload {
@@ -61,17 +62,20 @@ namespace crow {
                 res.end();
             };
 
-            std::string filepath("/var/lib/obmc/" + filename);           
-            if(access(filepath.c_str(), F_OK) != 0) {  ///file not exist, write file
-                BMCWEB_LOG_DEBUG << "Writing file to " << filepath;
-                std::ofstream out(filepath, std::ofstream::out | std::ofstream::binary | std::ofstream::trunc);
+            std::string filePath("/var/lib/obmc/" + filename); 
+            std::string tmpDirPath("/var/lib/obmc/");
+            if(access(filePath.c_str(), F_OK) != 0) {  ///file not exist, write file
+                BMCWEB_LOG_DEBUG << "Writing file to " << filePath;
+                std::ofstream out(filePath, std::ofstream::out | std::ofstream::binary | std::ofstream::trunc);
                 out << req.body;
                 out.close();
                 timeout.async_wait(timeoutHandler);
                 struct stat statbuff;
-                if((access(filepath.c_str(), F_OK) == 0) && 
-                    (stat(filepath.c_str(), &statbuff) == 0) && 
+                if((access(filePath.c_str(), F_OK) == 0) && 
+                    (stat(filePath.c_str(), &statbuff) == 0) && 
                     (statbuff.st_size == req.body.size())) { /// file exist and size equal req.body
+                    
+                    execl("/bin/tar", "tar", "-xf", filePath.c_str(), "-C", tmpDirPath.c_str(), (char*)0);
                     
                     res.jsonValue = {
                         {"data", nullptr},
